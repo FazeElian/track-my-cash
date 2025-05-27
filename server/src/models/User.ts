93dc48b54bb818a34eb35c401ea73d616eadca7d
FazeElian <elian.ibarra@soy.sena.edu.co>
2025-05-27 15:26:12 -0500

    add: User model

---
+++ server/src/models/User.ts
@@ -0,0 +1,65 @@
+import {
+    Table,
+    Column,
+    Model,
+    DataType,
+    Default,
+    Unique,
+    AllowNull,
+} from "sequelize-typescript";
+
+@Table({
+    tableName: "users"
+})
+
+class User extends Model {
+    @AllowNull(false)
+    @Column({
+        type: DataType.STRING(50)
+    })
+    declare userName: string
+
+    @AllowNull(false)
+    @Unique(true)
+    @Column({
+        type: DataType.STRING(60)
+    })
+    declare email: string
+
+    @AllowNull(false)
+    @Column({
+        type: DataType.STRING(60)
+    })
+    declare password: string
+
+    @AllowNull(true)
+    @Column({
+        type: DataType.STRING(60)
+    })
+    declare name: string
+
+    @AllowNull(true)
+    @Column({
+        type: DataType.STRING(60)
+    })
+    declare lastName: string
+
+    @AllowNull(true)
+    @Column({
+        type: DataType.STRING(60)
+    })
+    declare profilePhoto: string
+
+    @Column({
+        type: DataType.STRING(6)
+    })
+    declare token: string
+
+    @Default(false)
+    @Column({
+        type: DataType.BOOLEAN
+    })
+    declare confirmed: boolean
+}
+
+export default User;
