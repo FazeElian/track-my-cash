b988a9a383ea695872721a0203764688b50ad5a2
FazeElian <elian.ibarra@soy.sena.edu.co>
2025-06-01 22:27:29 -0500

    add: div when there's no categories

--- client/src/views/admin/categories/CategoriesGallery.tsx
+++ client/src/views/admin/categories/CategoriesGallery.tsx
@@ -21,58 +21,70 @@ const CategoriesGallery = () => {
     // Categories list from query
     const categories = useGetAllCategories()
 
-    if (Array.isArray(categories)) {
+    // Check if it's an array
+    if (!Array.isArray(categories)) {
+        return null;
+    }
+
+    // Check if it has categories
+    if (categories.length === 0) {
         return (
-            <section className="categories-gallery">
-                {categories.map((category: Category) => (
-                    <motion.div
-                        className="item-categories-gallery"
-                        whileHover={{ scale: 1.05 }}
-                        transition={{
-                            duration: .25,
-                        }}
-                    >
-                        <div className="top-item-categories-gallery">
-                            <div
-                                className={`icon-top-item-categories-gallery ${colorClassMap[category.color]}`}
-                            >
-                                {(() => {
-                                    const Icon = iconsMap[category.icon];
-                                    return Icon ? <Icon /> : <IoMdCart />;
-                                })()}
-                            </div>
-                            <div className="info-top-item-categories-gallery">
-                                <h1>{category.name}</h1>
-                                <div className="type-top-item-categories-gallery">
-                                    {category.type === "Expense" ? "Gasto" : "Ingreso"}
-                                </div>
-                            </div>
-                            <button
-                                type="button"
-                                className="btn-options-top-item-categories-gallery"
-                            >
-                                <MdOutlineMoreHoriz />
-                            </button>
-                        </div>
-                        <div className="txt-top-item-categories-gallery">
-                            <li className="item-txt-top-item-categories-gallery">
-                                <h2>Transacciones</h2>
-                                <h3>0</h3>
-                            </li>
-                            <li className="item-txt-top-item-categories-gallery">
-                                <h2>Presupuesto</h2>
-                                <h3>$ {category.monthlyBudget}</h3>
-                            </li>
-                            <li className="item-txt-top-item-categories-gallery">
-                                <h2>Gastado</h2>
-                                <h3>$ 0</h3>
-                            </li>
-                        </div>
-                    </motion.div>
-                ))}
-            </section>
+            <div className="no-data">
+                Aún no has añadido ninguna categoría.
+            </div>
         )
     }
+
+    return (
+        <section className="categories-gallery">
+            {categories.map((category: Category) => (
+                <motion.div
+                    className="item-categories-gallery"
+                    whileHover={{ scale: 1.05 }}
+                    transition={{
+                        duration: .25,
+                    }}
+                >
+                    <div className="top-item-categories-gallery">
+                        <div
+                            className={`icon-top-item-categories-gallery ${colorClassMap[category.color]}`}
+                        >
+                            {(() => {
+                                const Icon = iconsMap[category.icon];
+                                return Icon ? <Icon /> : <IoMdCart />;
+                            })()}
+                        </div>
+                        <div className="info-top-item-categories-gallery">
+                            <h1>{category.name}</h1>
+                            <div className="type-top-item-categories-gallery">
+                                {category.type === "Expense" ? "Gasto" : "Ingreso"}
+                            </div>
+                        </div>
+                        <button
+                            type="button"
+                            className="btn-options-top-item-categories-gallery"
+                        >
+                            <MdOutlineMoreHoriz />
+                        </button>
+                    </div>
+                    <div className="txt-top-item-categories-gallery">
+                        <li className="item-txt-top-item-categories-gallery">
+                            <h2>Transacciones</h2>
+                            <h3>0</h3>
+                        </li>
+                        <li className="item-txt-top-item-categories-gallery">
+                            <h2>Presupuesto</h2>
+                            <h3>$ {category.monthlyBudget}</h3>
+                        </li>
+                        <li className="item-txt-top-item-categories-gallery">
+                            <h2>Gastado</h2>
+                            <h3>$ 0</h3>
+                        </li>
+                    </div>
+                </motion.div>
+            ))}
+        </section>
+    )
 }
 
 export { CategoriesGallery };
