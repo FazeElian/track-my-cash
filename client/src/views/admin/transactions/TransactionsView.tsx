98b8bb73d8d5cd844ba937694bed87c066b5ad1e
FazeElian <elian.ibarra@soy.sena.edu.co>
2025-06-04 21:51:00 -0500

    add: Top components for transactions module main view

--- client/src/views/admin/transactions/TransactionsView.tsx
+++ client/src/views/admin/transactions/TransactionsView.tsx
@@ -1,7 +1,28 @@
+// Components for this view
+import { TopViewModule } from "../../../components/admin/TopTitle"
+import { SearchBar } from "../../../components/admin/SearchBar"
+
+// React icons
+import { IoMdSwap } from "react-icons/io"
+
 const TransactionsView = () => {
     return (
         <main className="content-page--admin">
-            <h1>Transactions View</h1>
+            <TopViewModule
+                title="Gestión de movimientos"
+                icon={IoMdSwap}
+                txtBtnAdd="Registrar movimiento"
+                txtBtnAddShort="Registrar"
+                btnAddOnClick={() => console.log()}
+                quickState1Value={`${12} movimientos registrador`}
+                quickState2Value={`${4} de ingresos`}
+                quickState3Value={`${8} de gastos`}
+            />
+            <SearchBar
+                titleModule="Movimientos"
+                searchName="transactions"
+                placeholder="Buscar movimiento por título"
+            />
         </main>
     )
 }
