d53030afb5cb129398eb8b110ec161f31034bc38
FazeElian <elian.ibarra@soy.sena.edu.co>
2025-07-04 17:35:00 -0500

    add: finance bot (cashy) section

--- client/src/components/admin/molecules/DashboardCharts.tsx
+++ client/src/components/admin/molecules/DashboardCharts.tsx
@@ -1,8 +1,12 @@
 // Styles for this component
 import "../../../assets/css/components/admin/DashboardCharts.css";
 
+// Images
+import BotImg  from "../../../assets/img/Bot.png";
+
 // Components
 import MonthlySummary from "./MonthlySummary";
+
 const DashboardCharts = () => {
     return (
         <section className="dashboard-charts">
@@ -11,9 +15,15 @@ const DashboardCharts = () => {
                 <h2>Ingresos vs Gastos en los últimos 6 meses</h2>
                 <MonthlySummary />
             </div>
-            <div className="item-dashboard-charts">
-                <h1>Categorías de gastos</h1>
-                <h2>Distribución de tus gastos por categoría</h2>
+            <div className="item-bot-dashboard-charts item-dashboard-charts">
+                <img src={BotImg} alt="Cashy bot" />
+                <h1>Tu Asistente Financiero Inteligente</h1>
+                <h2>Habla con <b>Cashy</b> sobre tus ingresos, metas y gastos.
+                    Resuelve tus dudas y mejora tus decisiones al instante.
+                </h2>
+                <button type="button" className="font-lexend">
+                    Hablar con Cashy
+                </button>
             </div>
         </section>
     )
