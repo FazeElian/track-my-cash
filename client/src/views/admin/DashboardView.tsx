ae6130b9aa3d968d915e47610de1f81c09174343
FazeElian <elian.ibarra@soy.sena.edu.co>
2025-07-06 19:22:29 -0500

    add: close new transaction modal form func on dashboard view

--- client/src/views/admin/DashboardView.tsx
+++ client/src/views/admin/DashboardView.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 // Lucide react icons
 import { DashboardIcon } from "../../lib/lists/Icons";
@@ -19,6 +19,29 @@ const DashboardView = () => {
     // New transaction form
     const [newTransactionForm, setNewTransactionForm] = useState(false)
     const formRef = useRef<HTMLFormElement>(null);
+
+    // Close the modal when user clicks outside the form
+    useEffect(() => {
+        const handleClickOutside = (event: MouseEvent) => {
+            if (formRef.current && !formRef.current.contains(event.target as Node)) {
+                setNewTransactionForm(false);
+            }
+        };
+
+        if (newTransactionForm) {
+            document.addEventListener("mousedown", handleClickOutside);
+
+            // Remove scroll on body
+            document.body.classList.add("no-scroll");
+            return () => document.body.classList.remove("no-scroll");
+        } else {
+            document.removeEventListener("mousedown", handleClickOutside);
+        }
+
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, [newTransactionForm]);
 
     return (
         <main className="content-page--admin">
