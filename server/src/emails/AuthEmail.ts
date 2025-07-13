0ccd669dd8d3ce10599b64430162ba4a660a04f3
FazeElian <elian.ibarra@soy.sena.edu.co>
2025-07-13 15:35:34 -0500

    fix(emails): styles & url for button

--- server/src/emails/AuthEmail.ts
+++ server/src/emails/AuthEmail.ts
@@ -12,7 +12,7 @@ export class AuthEmail {
     static sendConfirmationEmail = async (user: EmailType) => {
         // const templatePath = path.join(__dirname, "templates", "confirm-account.html");
         // let htmlTemplate = fs.readFileSync(templatePath, "utf8");
-        const url = `${process.env.VITE_URL_PROD}/auth/confirm-account`
+        const url = `${process.env.VITE_CUSTOM_URL}auth/confirm-account`
         const domain = process.env.EMAIL_DOMAIN;
 
         // htmlTemplate = htmlTemplate
@@ -43,20 +43,20 @@ export class AuthEmail {
                                 <td align="center" style="padding: 40px 20px;">
                                     <img src="https://track-my-cash.netlify.app/assets/Logo_Opt-XtX21Eq5.jpg" alt="Logo Track My Cash" width="300" style="display:block; border-bottom:2px solid #24BF67; padding-bottom:20px; background: #fff">
                                     <h1 style="font-size:22px; font-weight:normal; margin:30px 0 10px; color: #000; font-family: 'Lexend', Arial, sans-serif;">Hola ${user.userName},</h1>
-                                    <p style="font-size:16px; line-height:1.5; max-width:600px; margin:0 auto 20px; color: #000; font-family: 'Lexend', Arial, sans-serif;">
+                                    <p style="font-size:16px; line-height:1.5; max-width:600px; margin:0 auto 0; color: #000; font-family: 'Lexend', Arial, sans-serif;">
                                         ¡Bienvenido a <strong>Track My Cash! </strong>Confirma tu correo electrónico para activar tu cuenta.
                                         <br>Haz clic en el enlace de abajo e introduce el código para completar tu registro:
                                     </p>
 
-                                    <div style="display:inline-block; margin:20px 0; padding:12px 20px; background-color:#2EA662; color:#fff; font-size:22px; font-weight:bold; border-radius:5px font-family: 'Lexend', Arial, sans-serif;;">
-                                        CÓDIGO: ${user.code}
+                                    <div style="display:inline-block; margin:0; padding:12px 20px; background-color: #24BF67; color:#fff; font-size:28px; font-weight:bold; border-radius:10px font-family: 'Lexend', Arial, sans-serif;">
+                                        <h2 style="display: inline-block; color: white; background-color: #24BF67; padding: 1rem 2rem 1rem; font-size:24px; font-weight:bold; border-radius:10px;">
+                                            CÓDIGO: ${user.code}
+                                        <h2>
                                     </div>
 
-                                    <p style="margin:20px 0;">
                                     <a href="${url}" style="display:inline-block; padding:12px 24px; background-color:#3D90D9; color:white; text-decoration:none; font-size:16px; border-radius:8px; font-family: 'Lexend', Arial, sans-serif;">
                                         Confirmar mi cuenta
                                     </a>
-                                    </p>
                                 </td>
                             </tr>
                         </table>
@@ -71,7 +71,7 @@ export class AuthEmail {
     static sendForgotPasswordEmail = async (user: EmailType) => {
         // const templatePath = path.join(__dirname, "templates", "forgot-password.html");
         // let htmlTemplate = fs.readFileSync(templatePath, "utf8");
-        const url = `${process.env.VITE_URL_PROD}/auth/validate-code`
+        const url = `${process.env.VITE_CUSTOM_URL}auth/validate-code`
         const domain = process.env.EMAIL_DOMAIN;
 
         // htmlTemplate = htmlTemplate
@@ -102,20 +102,20 @@ export class AuthEmail {
                                 <td align="center" style="padding: 40px 20px;">
                                     <img src="https://track-my-cash.netlify.app/assets/Logo_Opt-XtX21Eq5.jpg" alt="Logo Track My Cash" width="300" style="display:block; border-bottom:2px solid #24BF67; padding-bottom:20px; background: #fff">
                                     <h1 style="font-size:22px; font-weight:normal; margin:30px 0 10px; color: #000; font-family: 'Lexend', Arial, sans-serif;">Hola ${user.userName},</h1>
-                                    <p style="font-size:16px; line-height:1.5; max-width:600px; margin:0 auto 20px; color: #000; font-family: 'Lexend', Arial, sans-serif;">
+                                    <p style="font-size:16px; line-height:1.5; max-width:600px; margin:0 auto 0; color: #000; font-family: 'Lexend', Arial, sans-serif;">
                                         Recientemente solicitaste restablecer tu contraseña para tu cuenta de <strong>Track My Cash</strong>.
-                                    <br>Usa el siguiente código para restablecerla:
+                                        <br>Usa el siguiente código para restablecerla:
                                     </p>
 
-                                    <div style="display:inline-block; margin:20px 0; padding:12px 20px; background-color:#2EA662; color:#fff; font-size:22px; font-weight:bold; border-radius:5px font-family: 'Lexend', Arial, sans-serif;;">
-                                        CÓDIGO: ${user.code}
+                                    <div style="display:inline-block; margin:0; padding:12px 20px; background-color: #24BF67; color:#fff; font-size:28px; font-weight:bold; border-radius:10px font-family: 'Lexend', Arial, sans-serif;">
+                                        <h2 style="display: inline-block; color: white; background-color: #24BF67; padding: 1rem 2rem 1rem; font-size:24px; font-weight:bold; border-radius:10px;">
+                                            CÓDIGO: ${user.code}
+                                        <h2>
                                     </div>
 
-                                    <p style="margin:20px 0;">
-                                    <a href="${url}" style="display:inline-block; padding:12px 24px; background-color:#3D90D9; color:white; text-decoration:none; font-size:16px; border-radius:8px; font-family: 'Lexend', Arial, sans-serif;">
+                                    <a href="${url}" style="display:inline-block; padding:12px 24px; background-color:#3D90D9; color:white; text-decoration:none; font-size:18px; border-radius:12px; font-weight: 500; font-family: 'Lexend', Arial, sans-serif;">
                                         Reestablecer mi contraseña
                                     </a>
-                                    </p>
                                 </td>
                             </tr>
                         </table>
